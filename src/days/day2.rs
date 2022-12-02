f8e1302291fd757dcdd0494f373a105779ed9be0
FailedCode <xeres666@googlemail.com>
2022-12-02 09:18:13 +0100

    Day2, part2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -41,6 +41,40 @@ pub fn solve_part_1() -> String {
     points.to_string()
 }
 
+// X - lose
+// Y - draw
+// Z - win
 pub fn solve_part_2() -> String {
-    "TODO: implement part 2".to_string()
+    let input_rows = crate::utils::load_input_list(2);
+    let mut points:u32 = 0;
+
+    let mut scores_play = HashMap::new();
+    scores_play.insert(String::from("A"), 1);
+    scores_play.insert(String::from("B"), 2);
+    scores_play.insert(String::from("C"), 3);
+
+    let mut scores_outcome = HashMap::new();
+    scores_outcome.insert(String::from("X"), 0);
+    scores_outcome.insert(String::from("Y"), 3);
+    scores_outcome.insert(String::from("Z"), 6);
+
+    let mut moves = HashMap::new();
+    moves.insert(String::from("AX"), String::from("C"));
+    moves.insert(String::from("AY"), String::from("A"));
+    moves.insert(String::from("AZ"), String::from("B"));
+    moves.insert(String::from("BX"), String::from("A"));
+    moves.insert(String::from("BY"), String::from("B"));
+    moves.insert(String::from("BZ"), String::from("C"));
+    moves.insert(String::from("CX"), String::from("B"));
+    moves.insert(String::from("CY"), String::from("C"));
+    moves.insert(String::from("CZ"), String::from("A"));
+
+    for row in input_rows {
+        if let Some((move_enemy, outcome)) = row.split_whitespace().collect_tuple()  {
+            let move_myself = moves.get(&[move_enemy, outcome].join("")).unwrap();
+            points += scores_play.get(move_myself).copied().unwrap_or(0);
+            points += scores_outcome.get(outcome).copied().unwrap_or(0);
+        }
+    }
+    points.to_string()
 }
